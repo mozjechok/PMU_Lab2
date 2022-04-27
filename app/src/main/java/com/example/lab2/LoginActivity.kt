82265b990fa5c6aa2ab364b3b9efb736c921dd7b
Max Melnikov <mml@highcore.io>
2022-04-27 21:42:10 +0300

    deleted register button

--- app/src/main/java/com/example/lab2/LoginActivity.kt
+++ app/src/main/java/com/example/lab2/LoginActivity.kt
@@ -8,22 +8,12 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import io.github.rybalkinsd.kohttp.dsl.httpPost
-import io.github.rybalkinsd.kohttp.ext.asString
-import io.github.rybalkinsd.kohttp.ext.url
 import kotlinx.coroutines.launch
-import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        val registerBtn = findViewById<Button>(R.id.RegisterBtn)
-        registerBtn.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
-        }
 
         val loginBtn = findViewById<Button>(R.id.LoginBtn)
         loginBtn.setOnClickListener {
@@ -35,35 +25,6 @@ class LoginActivity : AppCompatActivity() {
 
                 val login = findViewById<EditText>(R.id.LoginEdit).text.toString()
                 val password = findViewById<EditText>(R.id.PasswordEdit).text.toString()
-
-                /*val response = httpPost {
-                    url("http://192.168.0.112:8000/api/v1/getUser")
-
-                    body {
-                        json {
-                            "login" to login
-                            "password" to password
-                        }
-                    }
-
-                    header {
-                        "Content-Type" to "application/json"
-                    }
-                }
-
-                val obj = JSONObject(response.asString())
-                if (obj["result"].toString() == "" || obj["result"].toString()  == "[]") {
-                    findViewById<TextView>(R.id.LoginErrorText).text = "неверный логин/пароль"
-                } else {
-                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
-
-                    var user: User = User()
-                    user.login = login
-                    user.password = password
-                    intent.putExtra(User::class.java.simpleName, user)
-                    startActivity(intent)
-                }*/
 
                 if(login == "admin" && password == "admin")
                 {
